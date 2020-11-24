b5c2a1d4e2898e9dc9abe57e24843285db153df0
Samuel Miranda <x03314511159@tcu.gov.br>
2020-11-23 22:23:49 -0300

    adding login features.

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
@@ -1,15 +1,31 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
+    private final UserService userService;
+
+    public LoginController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
     public String loginView() {
+        return "login";
+    }
+
+    public String loginUser(@ModelAttribute User user, Model model) {
+        String loginError = null;
+
         return "login";
     }
 }
