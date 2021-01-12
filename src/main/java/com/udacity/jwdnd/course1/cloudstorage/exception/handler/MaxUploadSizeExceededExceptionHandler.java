93e1e70e040a29455edc3726885bc7cb54009d88
Samuel Miranda <x03314511159@tcu.gov.br>
2021-01-12 17:17:14 -0300

    changing MaxUploadSizeExceededExceptionHandler

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/exception/handler/MaxUploadSizeExceededExceptionHandler.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/exception/handler/MaxUploadSizeExceededExceptionHandler.java
@@ -3,16 +3,23 @@ package com.udacity.jwdnd.course1.cloudstorage.exception.handler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @ControllerAdvice
-public class MaxUploadSizeExceededExceptionHandler extends ResponseEntityExceptionHandler {
+public class MaxUploadSizeExceededExceptionHandler {
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public String handleMaxUploadSizeExceededExceptionHandler(MaxUploadSizeExceededException exception, RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("errorMessage", "Maximum upload size exceeded. ");
-        return "redirect:/result";
+    public ModelAndView handleMaxSizeException(MaxUploadSizeExceededException exception,
+                                                              HttpServletRequest request,
+                                                              HttpServletResponse response) {
+        ModelAndView modelAndView = new ModelAndView("result");
+        modelAndView.getModel().put("errorMessage", "Maximum upload size exceeded (limit 10MB). ");
+        return modelAndView;
     }
 
 
